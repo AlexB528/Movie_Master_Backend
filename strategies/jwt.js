cd540e2bb406b6101e2aaa65eb0c2493f357581c
Alex Brik <115663358+AlexB528@users.noreply.github.com>
2023-11-16 15:22:43 -0500

    update

--- strategies/jwt.js
+++ strategies/jwt.js
@@ -4,8 +4,11 @@ const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = 'SECRET_KEY'; //normally store this in process.env.secret
 
-module.exports = new JwtStrategy(opts, (jwt_payload, done) => {
-    if (jwt_payload.username === "TBrown88") {
+const userDetails = await User.find({ username: username }).exec();
+
+module.exports = new JwtStrategy(opts, async (jwt_payload, done) => {
+    user = await User.find({ username: jwt_payload.username }).exec();
+    if (user) {
         return done(null, true)
     }
     return done(null, false)
