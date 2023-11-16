5a3e1ff53733f7def6590f99374102e40014bbb5
Alex Brik <115663358+AlexB528@users.noreply.github.com>
2023-11-16 14:21:20 -0500

    update jwt strategy

--- strategies/jwt.js
+++ strategies/jwt.js
@@ -1,12 +1,33 @@
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = 'SECRET_KEY'; //normally store this in process.env.secret
+// const JwtStrategy = require('passport-jwt').Strategy;
+// const ExtractJwt = require('passport-jwt').ExtractJwt;
+// const opts = {}
+// opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+// opts.secretOrKey = 'SECRET_KEY'; //normally store this in process.env.secret
 
-module.exports = new JwtStrategy(opts, (jwt_payload, done) => {
-    if (jwt_payload.username === "TBrown88") {
-        return done(null, true)
-    }
-    return done(null, false)
-}) 
+// module.exports = new JwtStrategy(opts, (jwt_payload, done) => {
+//     if (jwt_payload.username === "TBrown88") {
+//         return done(null, true)
+//     }
+//     return done(null, false)
+// })
+
+var JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+var opts = {}
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = 'SECRET_KEY';
+// opts.issuer = 'accounts.examplesoft.com';
+// opts.audience = 'yoursite.net';
+passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+    User.findOne({id: jwt_payload.sub}, function(err, user) {
+        if (err) {
+            return done(err, false);
+        }
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+            // or you could create a new account
+        }
+    });
+}));
