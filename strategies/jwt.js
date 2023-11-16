e6ae9d905981fad1b8714b577a276bc1fe39edce
Alex Brik <115663358+AlexB528@users.noreply.github.com>
2023-11-16 15:35:55 -0500

    update

--- strategies/jwt.js
+++ strategies/jwt.js
@@ -1,11 +1,26 @@
+// const JwtStrategy = require('passport-jwt').Strategy;
+// const ExtractJwt = require('passport-jwt').ExtractJwt;
+// const opts = {}
+// opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+// opts.secretOrKey = 'SECRET_KEY'; //normally store this in process.env.secret
+
+// module.exports = new JwtStrategy(opts, (jwt_payload, done) => {
+    
+//     if (jwt_payload.username === "BRover11") {
+//         return done(null, true)
+//     }
+//     return done(null, false)
+// })
+
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const opts = {}
 opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = 'SECRET_KEY'; //normally store this in process.env.secret
 
-module.exports = new JwtStrategy(opts, (jwt_payload, done) => {
-    if (jwt_payload.username === "BRover11") {
+module.exports = new JwtStrategy(opts, async (jwt_payload, done) => {
+    const user = await User.findOne({ username: jwt_payload.username }).exec();
+    if (user) {
         return done(null, true)
     }
     return done(null, false)
