06a11e589d37dd09b6187ac61ad78794f6cfff91
Alex Brik <115663358+AlexB528@users.noreply.github.com>
2023-11-16 13:41:10 -0500

    update

--- strategies/jwt.js
+++ strategies/jwt.js
@@ -5,7 +5,6 @@ opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = 'SECRET_KEY'; //normally store this in process.env.secret
 
 module.exports = new JwtStrategy(opts, (jwt_payload, done) => {
-    console.log(Cookies.get('user'));
     if (jwt_payload.username === "TBrown88") {
         return done(null, true)
     }
